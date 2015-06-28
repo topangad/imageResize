c56b7c63e8e84f47b950d3573537668cdd84e88c
topangad <olga.bool@yahoo.ca>
2015-06-28 05:02:51 -0400

    finished energy function

--- SeamCarver.java
+++ SeamCarver.java
@@ -1,6 +1,7 @@
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.awt.Color;
 
 public class SeamCarver {
 
@@ -20,8 +21,36 @@ public class SeamCarver {
       return this.picture.height();
    }
    public  double energy(int x, int y){
-     //calculating the energy of a pixel
-      return 0;
+      Color top, bottom, left, right;
+      double dx, dy, energy;
+      double rdx, bdx, gdx, rdy, bdy, gdy;
+      
+      if ( x == 0 || y == 0 ||
+            x == picture.width()-1 ||
+            y == picture.height()-1){      
+         top = this.picture.get(x, y);
+         bottom = top;
+         left = top;
+         right = top;         
+      } else { 
+         top = this.picture.get(x, y-1);
+         bottom = this.picture.get(x, y+1);
+         right = this.picture.get(x+1, y);
+         left = this.picture.get(x-1, y);
+      }
+      
+      rdx = right.getRed() - left.getRed();
+      bdx = right.getBlue() - left.getBlue();
+      gdx = right.getGreen() - left.getGreen();
+
+      rdy = top.getRed() - bottom.getRed();
+      bdy = top.getBlue() - bottom.getBlue();
+      gdy = top.getGreen() - bottom.getGreen(); 
+      
+      dx = rdx*rdx + bdx*bdx + gdx*gdx;
+      dy = rdy*rdy + bdy*bdy + gdy*gdy;      
+      
+      return dx + dy;
   }
    public   int[] findHorizontalSeam(){
    // sequence of indices for horizontal seam
