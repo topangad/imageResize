9ebeaf49c80af7f0434a736fd70a0bf33f749851
topangad <olga.bool@yahoo.ca>
2015-07-22 12:27:19 -0400

    Fixed energy calculations and horiz seam calculations

--- SeamCarver.java
+++ SeamCarver.java
@@ -23,29 +23,21 @@ public class SeamCarver {
    public  double energy(int x, int y){
       Color top, bottom, left, right;
       double dx, dy, energy;
-      double rdx, bdx, gdx, rdy, bdy, gdy;
+      double rdx, bdx, gdx, rdy, bdy, gdy;      
       
-      if ( x == 0 || y == 0 ||
-            x >= picture.width()-1 ||
-            y >= picture.height()-1){      
-         top = this.picture.get(x, y);
-         bottom = top;
-         left = top;
-         right = top;         
-      } else { 
-         top = this.picture.get(x, y-1);
-         bottom = this.picture.get(x, y+1);
-         right = this.picture.get(x+1, y);
-         left = this.picture.get(x-1, y);
-      }
+      //handles boundary pixels
+       top = this.picture.get(x,( y == 0 ) ? height()-1 : y - 1);
+       bottom = this.picture.get(x, ( y == height()-1 ) ? 0 : y + 1);
+       left = this.picture.get( ( x == 0 ) ? width()-1 : x - 1, y);
+       right = this.picture.get( ( x == width()-1) ? 0 : x + 1, y);     
       
-      rdx = right.getRed() - left.getRed();
-      bdx = right.getBlue() - left.getBlue();
-      gdx = right.getGreen() - left.getGreen();
+      rdx = Math.abs(right.getRed() - left.getRed());
+      bdx = Math.abs(right.getBlue() - left.getBlue());
+      gdx = Math.abs(right.getGreen() - left.getGreen());
 
-      rdy = top.getRed() - bottom.getRed();
-      bdy = top.getBlue() - bottom.getBlue();
-      gdy = top.getGreen() - bottom.getGreen(); 
+      rdy = Math.abs(top.getRed() - bottom.getRed());
+      bdy = Math.abs(top.getBlue() - bottom.getBlue());
+      gdy = Math.abs(top.getGreen() - bottom.getGreen()); 
       
       dx = rdx*rdx + bdx*bdx + gdx*gdx;
       dy = rdy*rdy + bdy*bdy + gdy*gdy;      
@@ -85,40 +77,47 @@ public class SeamCarver {
    }
    
    public int[] getMinSeam(double [][] grid) {
-      int rows=grid.length;
-      System.out.println("rows: "+ rows);
+      int rows=grid.length;      
       int cols=grid[0].length;
       int[] hseam = new int[cols];
       double min = grid [0][cols-1];
       int minIndex = 0;
       
-      for (int i = 1 ; i < rows; i ++){
+      for (int i = 1 ; i < rows; i++){
          if(grid[i][cols-1] < min) {
             min = grid[i][cols-1];
             minIndex = i;
          }
       }
       
-      for ( int i = cols-1 ; i >= 1 ; i --) {
+      for ( int i = cols-1 ; i >= 1 ; i--) {
       //swap x and y for rows and cols, minIndex represents they coordinate
          double curEnergy = energy(i, minIndex);
-         System.out.println("row " + i);
+         
          double target = grid[minIndex][i] - curEnergy;
          
          hseam[i] = minIndex;
          //have to do edge cases check
-         if(minIndex >=1) {
-            if(grid[minIndex-1][i-1] == target) {
-               minIndex = minIndex -1;
-            }
-         }
-         
-         if(minIndex < rows-1) {
+         if(minIndex == 0) {            
             if(grid[minIndex+1][i-1] == target) {
-               minIndex = minIndex -1;
+               minIndex = minIndex +1;
+            }            
+         }
+         else if (minIndex == rows-1) {
+            if(grid[minIndex-1][i-1] == target) {
+               minIndex = minIndex-1;
             }
          }
+         else {
+            if(grid[minIndex+1][i-1] == target) {
+               minIndex = minIndex+1;
+            }  else if(grid[minIndex-1][i-1] == target) {
+               minIndex = minIndex-1;
+            }
+
+         }
          
+                  
       }
       hseam[0] = minIndex;
 
@@ -126,6 +125,9 @@ public class SeamCarver {
    }
    public   int[] findVerticalSeam(){
    // sequence of indices for vertical seam
+      int [] vseam = new int [2];
+      vseam[0]=1;
+      vseam[1]=2;
       return null;
    }
    public void removeHorizontalSeam(int[] seam){   // remove horizontal seam from current picture
